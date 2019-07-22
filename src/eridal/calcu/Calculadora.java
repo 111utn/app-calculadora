daafc38c836d3d19cfec819e65191baf79743d94
Martin Scotta <martinscotta@gmail.com>
2019-07-22 19:20:31 -0300

    probando con errores simulados

--- src/eridal/calcu/Calculadora.java
+++ src/eridal/calcu/Calculadora.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Calculadora {
 
+  /**
+   * El resultado de evaluar el ingreso del usuario
+   */
+  private static String salida;
+
+  /**
+   * El tipo de error encontrado al evaluar el ingreso del usuario
+   */
+  private static String error;
+
+
   public static void main(String[] args) {
 
     final Scanner sc = new Scanner(System.in);
@@ -14,10 +25,45 @@ public class Calculadora {
 
       final String input = sc.nextLine();
 
-      System.out.println(input);
+      if (procesar(input)) {
+        System.out.println(salida);
+      }
+      else {
+        System.err.println(error);
+      }
+
       System.out.print("> ");
     }
 
     sc.close();
+  }
+
+  /**
+   * Solo para alternar entre error y resultado
+   */
+  private static boolean simularError = false;
+
+  /**
+   * Procesa la entrada del usuario y genera una salida
+   *
+   * Esta forma de proceso no es la ideal, pero aun no sabemos
+   * control de errores. Por ahora queda asi.
+   *
+   *
+   * @return boolean `true` si la entrada pudo ser analizada correctamente
+   */
+  private static boolean procesar(final String input) {
+
+    final boolean fueOK = !simularError;
+
+    if (fueOK) {
+      salida = input; // por ahora la salida es lo mismo que recibimos
+    } else {
+      error = "Error: probando errores"; // por ahora un unico mensaje de error
+    }
+
+    simularError = ! simularError;
+
+    return fueOK;
   }
 }
